ccf8d51e1ec47ba1b92a82443bf771ab4ae346a5
Jinyoung <roghabo@gmail.com>
2024-12-18 16:03:24 +0900

    9.3

---
+++ lib/features/discover/discover_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = [
+  "Top",
+  "Users",
+  "Videos",
+  "Sounds",
+  "LIVE",
+  "Shopping",
+  "Brands",
+];
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 7,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: const Text("Discover"),
+          bottom: TabBar(
+              tabAlignment: TabAlignment.start,
+              splashFactory: NoSplash.splashFactory,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size16,
+              ),
+              isScrollable: true,
+              labelStyle: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.grey.shade500,
+              indicatorColor: Colors.black,
+              tabs: [
+                for (var tab in tabs)
+                  Tab(
+                    text: tab,
+                  )
+              ]),
+        ),
+        body: TabBarView(children: [
+          GridView.builder(
+            itemCount: 20,
+            padding: const EdgeInsets.all(
+              Sizes.size6,
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: Sizes.size10,
+              mainAxisSpacing: Sizes.size10,
+              childAspectRatio: 9 / 16,
+            ),
+            itemBuilder: (context, index) => Container(
+              color: Colors.teal,
+              child: Center(
+                child: Text("$index"),
+              ),
+            ),
+          ),
+          for (var tab in tabs.skip(1))
+            Center(
+              child: Text(
+                tab,
+                style: const TextStyle(
+                  fontSize: 28,
+                ),
+              ),
+            )
+        ]),
+      ),
+    );
+  }
+}
