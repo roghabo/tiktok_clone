8ad4a459db2c05167e1015c254ef16e4562c1ad0
Jinyoung <roghabo@gmail.com>
2024-12-23 20:44:37 +0900

    13.1

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -6,13 +6,28 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Settings'),
-        ),
-        body: const Column(
-          children: [
-            CircularProgressIndicator.adaptive(),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => showAboutDialog(
+                context: context,
+                applicationVersion: "1.0",
+                applicationLegalese:
+                    "All rights reseverd. Please dont copy me."),
+            title: const Text(
+              "About",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: const Text("About this app....."),
+          ),
+          const AboutListTile(),
+        ],
+      ),
+    );
   }
 }
